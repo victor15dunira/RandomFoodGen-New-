f3d297278a385a807efb20ae4552f6900da0cbb4
victor15dunira <victor.dunira@avanade.com>
2021-06-23 10:36:17 +0100

    WorkFlow two/four

--- code/Frontend/Startup.cs
+++ code/Frontend/Startup.cs
@@ -13,18 +13,28 @@ namespace Frontend
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(env.ContentRootPath)
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
+
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting(r => r.LowercaseUrls = true);
             services.AddControllersWithViews();
+            services.Configure<AppSettings>(options => Configuration.GetSection("AppSettings").Bind(options));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- code/Meals.Web.Tests/Controllers/FrontEndControllerTest.cs
+++ code/Meals.Web.Tests/Controllers/FrontEndControllerTest.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Meals.Web.Tests.Controllers
-{
-    class FrontEndControllerTest
-    {
-    }
-}
+//namespace Meals.Web.Tests.Controllers
+//{
+//    class FrontEndControllerTest
+//    {
+
+
+//        private AppSettings appSettings = new AppSettings()
+//        {
+
+         
+//        };
+
+//        [Fact]
+//        public async void GetTest()
+//        {
+//            var options = new Mock<IOptions<AppSettings>>(); //MOCK of the merge controller directed from Appsettings azure URLS
+//            options.Setup(x => x.Value).Returns(appSettings);
+
+//            MergeController mergeController = new MergeController(options.Object); //injection of the mocked mergecontroller
+//            var mergeControllerResult = await mergeController.Get();
+
+//            Assert.NotNull(mergeControllerResult);
+//            Assert.IsType<OkObjectResult>(mergeControllerResult);
+
+
+
+//        }
+
+//    }
+//}
+
+
+
+
+
+    
+//}
